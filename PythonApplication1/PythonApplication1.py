f70a3cee8eae7750f81201aea42f9b4a3f616c38
Adrian Lutsyk <adrian2.lutsyk2010@gmail.com>
2025-07-14 13:14:51 +0200

    Optimising function is now optimising all of hte weights for one iteration

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -6,11 +6,12 @@ from activations import Activation_ReLU, Activatioin_Softmax
 from weightsBiases import Layer_Dense
 from losses import Loss_CategoricalCrossentrophy
 from iterate import iteration
-from logicManager import optimise
+from logicManager import Optimiser
 
 nnfs.init()
 iterations = 0
 loss = float('inf')
+change = 0.05
 
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
@@ -20,10 +21,12 @@ activation2 = Activatioin_Softmax()
 
 X, y = spiral_data(samples=100, classes=3)
 
-for i in range(1, 100000):
+optimiser = Optimiser(X, y, activation1, activation2, change)
+
+for i in range(0, 105):
     print(f"Iteration {iterations}")
     loss = iteration(X, y, dense1, dense2, activation1, activation2)
-    loss, dense1, dense2 = optimise(X, y, loss, dense1, dense2, iterations, activation1, activation2)
+    loss, dense1, dense2 = optimiser.optimise(loss, dense1, dense2, iterations)
     print(f"Loss {loss}")
     iterations +=1
     
--- PythonApplication1/logicManager.py
+++ PythonApplication1/logicManager.py
@@ -2,27 +2,49 @@ import numpy as np
 import copy
 from iterate import iteration
 
-def getWeightCords(weights, iteration):
-    d, r, c = len(weights), len(weights[0]), len(weights[0][0])
-    iteration %= d * r * c
-    return [iteration // (r * c), (iteration // c) % r, iteration % c]
+class Optimiser():
+    def __init__(self, X, y, activation1, activation2, change):
+        self.X = X
+        self.y = y
+        self.activation1 = activation1
+        self.activation2 = activation2
+        self.change = change
 
-def changeWeight(weights, iteration, change):
-    weightCords = getWeightCords(weights, iteration)
-    weights[weightCords[0]][weightCords[1]][weightCords[2]] += change
-    return weights[0], weights[1]
+    def getWeightCords(self, weights, nthElem):
+        d, r, c = len(weights), len(weights[0]), len(weights[0][0])
+        nthElem %= d * r * c
+        return [nthElem // (r * c), (nthElem // c) % r, nthElem % c]
 
-def optimise(X, y, loss, dense1, dense2, iterations, activation1, activation2):
-    positiveDense1, positiveDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
-    negativeDense1, negativeDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
+    def changeWeight(self, weights, nthElem, change):
+        weightCords = self.getWeightCords(weights, nthElem)
+        weights[weightCords[0]][weightCords[1]][weightCords[2]] += change
+        return weights[0], weights[1]
 
-    positiveDense1.weights, positiveDense2.weights = changeWeight([copy.deepcopy(dense1.weights), copy.deepcopy(dense2.weights)], iterations, 0.05)
-    negativeDense1.weights, negativeDense2.weights = changeWeight([copy.deepcopy(dense1.weights), copy.deepcopy(dense2.weights)], iterations, -0.05)
+    def optimiseSingleWeight(self, loss, positiveDense1, positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem):
+        positiveDense1.weights, positiveDense2.weights = self.changeWeight([copy.deepcopy(positiveDense1.weights), copy.deepcopy(positiveDense2.weights)], nthElem, 0.05)
+        negativeDense1.weights, negativeDense2.weights = self.changeWeight([copy.deepcopy(negativeDense1.weights), copy.deepcopy(negativeDense2.weights)], nthElem, -0.05)
 
-    positiveLoss = iteration(X, y, positiveDense1, positiveDense2, activation1, activation2)
-    negativeLoss = iteration(X, y, negativeDense1, negativeDense2, activation1, activation2)
+        positiveLoss = iteration(self.X, self.y, positiveDense1, positiveDense2, self.activation1, self.activation2)
+        negativeLoss = iteration(self.X, self.y, negativeDense1, negativeDense2, self.activation1, self.activation2)
 
-    losses = [(positiveLoss, positiveDense1, positiveDense2),
-              (negativeLoss, negativeDense1, negativeDense2),
-              (loss, dense1, dense2)]
-    return min(losses, key=lambda x: x[0]) 
+        losses = [(positiveLoss, positiveDense1, positiveDense2),
+                  (negativeLoss, negativeDense1, negativeDense2),
+                  (loss, dense1, dense2)]
+        return min(losses, key=lambda x: x[0]) 
+
+     
+    def optimise(self, loss, dense1, dense2, nthElem):
+        
+
+        for nthElem in range(sum(len(inner) for outer in [copy.deepcopy(dense1.weights), copy.deepcopy(dense2.weights)] for inner in outer)):
+            positiveDense1, positiveDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
+            negativeDense1, negativeDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
+
+            loss, dense1, dense2 = self.optimiseSingleWeight(loss, positiveDense1, positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem)
+
+        # positiveDense1, positiveDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
+        # negativeDense1, negativeDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
+
+        # loss, dense1, dense2 = self.optimiseSingleWeight(loss, positiveDense1, positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem)
+
+        return loss, dense1, dense2
