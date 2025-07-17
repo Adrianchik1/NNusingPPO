87bd6cc413956c1206fc419cc20bb17f300990dd
Adrian Lutsyk <adrian2.lutsyk2010@gmail.com>
2025-07-17 14:17:42 +0200

    Implemented Biases

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -4,7 +4,6 @@ from nnfs.datasets import spiral_data
 
 from activations import Activation_ReLU, Activatioin_Softmax
 from weightsBiases import Layer_Dense
-from losses import Loss_CategoricalCrossentrophy
 from iterate import iteration
 from logicManager import Optimiser
 
@@ -12,6 +11,10 @@ nnfs.init()
 iterations = 0
 loss = float('inf')
 change = 0.05
+losses = []         #array to store the last 10 losses
+progressOfLosses = []       #an array to store all losses(for future analysis)
+differenceOfLosses = []
+implementBiases = False
 
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
@@ -23,10 +26,34 @@ X, y = spiral_data(samples=100, classes=3)
 
 optimiser = Optimiser(X, y, activation1, activation2, change)
 
-for i in range(0, 105):
+for i in range(0, 1000):
     print(f"Iteration {iterations}")
     loss = iteration(X, y, dense1, dense2, activation1, activation2)
-    loss, dense1, dense2 = optimiser.optimise(loss, dense1, dense2, iterations)
+    loss, dense1, dense2 = optimiser.optimise(loss, dense1, dense2)
     print(f"Loss {loss}")
+
+    losses.append(loss)
+    #progressOfLosses.append(loss)
+    if len(losses) > 13: losses.pop(0)                                      # removes the oldest loss if it has more than 10 elements
+    if len(losses) > 10 and np.allclose(losses, losses[0]) == True:         # if loss hasnt changed in the last 10 iterations implements biases
+        #if implementBiases == True:                                         # if biases have already been implemented, then break
+        #     break
+        #implementBiases = True
+        progress = iterations - 10
+        break
+    if len(losses)>2 and i%10 == 0:
+        differenceOfLosses.append(losses[0] - losses[1])  # appends the change in loss to the progressOfLosses array)
+        progressOfLosses.append(loss)
+    # if len(losses)>2 and losses[0] - losses[-1] < 0.001:
+    #     optimiser.change /= 2
+
+
     iterations +=1
     
+
+
+print(loss)
+print(iterations)
+print(differenceOfLosses, "/n")
+print(progressOfLosses)
+input("Press enter to exit")
--- PythonApplication1/logicManager.py
+++ PythonApplication1/logicManager.py
@@ -1,3 +1,5 @@
+from re import A
+from xml.sax.handler import all_properties
 import numpy as np
 import copy
 from iterate import iteration
@@ -10,37 +12,48 @@ class Optimiser():
         self.activation2 = activation2
         self.change = change
 
-    def getWeightCords(self, weights, nthElem):
-        d, r, c = len(weights), len(weights[0]), len(weights[0][0])
-        nthElem %= d * r * c
-        return [nthElem // (r * c), (nthElem // c) % r, nthElem % c]
-
+    def getParameterCords(self, WeightsOrBiases, nthElem):
+        for x,m in enumerate(WeightsOrBiases):
+            r,c=len(m),len(m[0])
+            if nthElem<r*c:return[x,nthElem//c,nthElem%c]
+            nthElem-=r*c
     def changeWeight(self, weights, nthElem, change):
-        weightCords = self.getWeightCords(weights, nthElem)
+        weightCords = self.getParameterCords(weights, nthElem)
         weights[weightCords[0]][weightCords[1]][weightCords[2]] += change
-        return weights[0], weights[1]
+        return weights
 
-    def optimiseSingleWeight(self, loss, positiveDense1, positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem):
-        positiveDense1.weights, positiveDense2.weights = self.changeWeight([copy.deepcopy(positiveDense1.weights), copy.deepcopy(positiveDense2.weights)], nthElem, 0.05)
-        negativeDense1.weights, negativeDense2.weights = self.changeWeight([copy.deepcopy(negativeDense1.weights), copy.deepcopy(negativeDense2.weights)], nthElem, -0.05)
+    def optimiseSingleWeight(self, loss, positiveDense1 ,positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem):
+        positiveDense1.weights, positiveDense2.weights = self.changeWeight([copy.deepcopy(positiveDense1.weights), copy.deepcopy(positiveDense2.weights)], nthElem, self.change)
+        negativeDense1.weights, negativeDense2.weights = self.changeWeight([copy.deepcopy(negativeDense1.weights), copy.deepcopy(negativeDense2.weights)], nthElem, -self.change)
 
         positiveLoss = iteration(self.X, self.y, positiveDense1, positiveDense2, self.activation1, self.activation2)
         negativeLoss = iteration(self.X, self.y, negativeDense1, negativeDense2, self.activation1, self.activation2)
 
         losses = [(positiveLoss, positiveDense1, positiveDense2),
                   (negativeLoss, negativeDense1, negativeDense2),
-                  (loss, dense1, dense2)]
+                  (loss, dense1, dense2)] 
+        return min(losses, key=lambda x: x[0]) 
+
+    def optimiseSingleBias(self, loss, positiveDense1 ,positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem):
+        positiveDense1.biases, positiveDense2.biases = self.changeWeight([copy.deepcopy(positiveDense1.biases), copy.deepcopy(positiveDense2.biases)], nthElem, self.change)
+        negativeDense1.biases, negativeDense2.biases = self.changeWeight([copy.deepcopy(negativeDense1.biases), copy.deepcopy(negativeDense2.biases)], nthElem, -self.change)
+
+        positiveLoss = iteration(self.X, self.y, positiveDense1, positiveDense2, self.activation1, self.activation2)
+        negativeLoss = iteration(self.X, self.y, negativeDense1, negativeDense2, self.activation1, self.activation2)
+
+        losses = [(positiveLoss, positiveDense1, positiveDense2),
+                  (negativeLoss, negativeDense1, negativeDense2),
+                  (loss, dense1, dense2)] 
         return min(losses, key=lambda x: x[0]) 
 
      
-    def optimise(self, loss, dense1, dense2, nthElem):
-        
-
+    def optimise(self, loss, dense1, dense2): 
         for nthElem in range(sum(len(inner) for outer in [copy.deepcopy(dense1.weights), copy.deepcopy(dense2.weights)] for inner in outer)):
             positiveDense1, positiveDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
             negativeDense1, negativeDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
 
-            loss, dense1, dense2 = self.optimiseSingleWeight(loss, positiveDense1, positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem)
+            loss, dense1, dense2 = self.optimiseSingleWeight(loss, positiveDense1 ,positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem)
+            if nthElem%len(dense1.biases[0]) == 0: nthElem = int(nthElem/len(dense1.biases[0])); loss, dense1, dense2 = self.optimiseSingleBias(loss, positiveDense1 ,positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem)
 
         # positiveDense1, positiveDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
         # negativeDense1, negativeDense2 = copy.deepcopy(dense1), copy.deepcopy(dense2)
@@ -48,3 +61,4 @@ class Optimiser():
         # loss, dense1, dense2 = self.optimiseSingleWeight(loss, positiveDense1, positiveDense2, negativeDense1, negativeDense2, dense1, dense2, nthElem)
 
         return loss, dense1, dense2
+
